f7f1285efa19debf7dfe7c4b7a97f23e872d5cb5
Jonathan Sheely <jsheely@thirtytech.net>
2023-09-09 12:30:02 -0400

    Adding stats by author

--- Work.cs
+++ Work.cs
@@ -13,6 +13,16 @@ public static class Work
 			var branches = repo.Branches;
 			var authors = repo.Commits.Select(c => c.Author.Name).Distinct();
 			var linesChanged = repo.Commits.Where(c => c.Committer.When >= fromDate).Select(c => repo.Diff.Compare<Patch>(c.Parents.FirstOrDefault()?.Tree, c.Tree));
+			var commitsDoneByAuthor = repo.Commits.Where(c => c.Committer.When >= fromDate).GroupBy(c => c.Author.Name).Select(g => new { Author = g.Key, Commits = g.Count() });
+			var filesChangedByAuthor = repo.Commits.Where(c => c.Committer.When >= fromDate).GroupBy(c => c.Author.Name).Select(g => new { Author = g.Key, Files = g.SelectMany(c => c.Tree.Select(t => t.Path)).Distinct().Count() });
+			foreach (var file in filesChangedByAuthor)
+			{
+				Console.WriteLine(file.Author + ": " + file.Files);
+			}
+			foreach (var commit in commitsDoneByAuthor)
+			{
+				Console.WriteLine(commit.Author + ": " + commit.Commits);
+			}
 			foreach (var line in linesChanged)
 			{
 				Console.WriteLine(line.LinesAdded + line.LinesDeleted);
