192a5846d9b3aa9d317e588a1b331e243e3a69c1
Jonathan Sheely <jsheely@thirtytech.net>
2023-09-09 12:18:18 -0400

    Fixing directory paths with defaults

--- Program.cs
+++ Program.cs
@@ -1,33 +1,38 @@
 ﻿using System.CommandLine;
 using System.Diagnostics;
 using LibGit2Sharp;
-using Humanizer;
 
 var FromDate = new Option<string>("--fromDate", description: "Starting date for commits to be considered");
+var Folder = new Option<string>("--folder", description: "Folder to search for git repositories", getDefaultValue: () =>
+{
+	var directory = !Debugger.IsAttached
+		? Directory.GetCurrentDirectory()
+		: Directory.GetParent(Directory.GetCurrentDirectory())?.Parent?.Parent?.FullName;
+	return directory ?? "";
+});
 
 var root = new RootCommand {
-	FromDate
+	FromDate,
+	Folder
+
 };
 
-root.SetHandler(async (fromDate) =>
+root.SetHandler(async (folder, fromDate) =>
 {
-	if(TryParseHumanReadableTimeSpan(fromDate, out var timeSpan))
+	if (TryParseHumanReadableTimeSpan(fromDate, out var timeSpan))
 	{
-		DoWork(DateTime.Now - timeSpan);
+		DoWork(folder, DateTime.Now - timeSpan);
 	}
-}, FromDate);
+}, Folder, FromDate);
 
 await root.InvokeAsync(args);
 Console.WriteLine("Done");
 return;
 
-void DoWork(DateTime fromDate)
+void DoWork(string directory, DateTime fromDate)
 {
 
-	var directory = !Debugger.IsAttached
-		? Directory.GetCurrentDirectory()
-		: Directory.GetParent(Directory.GetCurrentDirectory())?.Parent?.Parent?.FullName;
-
+	Console.WriteLine("Processing directory: " + directory);
 
 	// Get current directory
 	using (var repo = new Repository(directory))
