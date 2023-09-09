60c2d6a6d7c94eca074f3f00ae0ce71663010957
Jonathan Sheely <jsheely@thirtytech.net>
2023-09-09 11:24:16 -0400

    Initial

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using LibGit2Sharp;
+
+// Get current directory
+using (var repo = new Repository(Directory.GetCurrentDirectory()))
+{
+	var branches = repo.Branches;
+	foreach (var branch in branches)
+	{
+		Console.WriteLine(branch.FriendlyName);
+	}
+}
