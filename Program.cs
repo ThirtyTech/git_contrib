bd1fa7478a333693bbb0d77c9d910c2930b37fb9
Jonathan Sheely <jsheely@thirtytech.net>
2023-09-09 11:38:54 -0400

    Updated with getting patches

--- Program.cs
+++ Program.cs
@@ -1,9 +1,22 @@
-﻿using LibGit2Sharp;
+﻿using System.Diagnostics;
+using LibGit2Sharp;
+
+// Use parent directory during debug. Check for if DEBUG
+var directory = !Debugger.IsAttached
+	? Directory.GetCurrentDirectory()
+	: Directory.GetParent(Directory.GetCurrentDirectory())?.Parent?.Parent?.FullName;
+
 
 // Get current directory
-using (var repo = new Repository(Directory.GetCurrentDirectory()))
+using (var repo = new Repository(directory))
 {
 	var branches = repo.Branches;
+	var authors = repo.Commits.Select(c => c.Author).Distinct();
+	var linesChangedByAuthor = repo.Commits.Select(c => repo.Diff.Compare<Patch>(c.Parents.First().Tree, c.Tree));
+	foreach (var author in authors)
+	{
+		Console.WriteLine(author.Name);
+	}
 	foreach (var branch in branches)
 	{
 		Console.WriteLine(branch.FriendlyName);
