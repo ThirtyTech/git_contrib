10c721453150ea3d68450deef3ada7158792a6aa
Jonathan Sheely <jsheely@thirtytech.net>
2023-12-28 11:20:30 -0500

    SelectMany to get individual unique file strings

--- Models/AuthorContrib.cs
+++ Models/AuthorContrib.cs
@@ -13,7 +13,7 @@ public class AuthorData
     public string Name { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public Dictionary<string, ChangeSet> ChangeMap { get; set; } = new Dictionary<string, ChangeSet>();
-    public int UniqueFiles => ChangeMap.Select(x => x.Value.FileItems).Distinct().Count();
+    public int UniqueFiles => ChangeMap.SelectMany(x => x.Value.FileItems).Distinct().Count();
     public int TotalCommits => ChangeMap.Sum(x => x.Value.Commits);
     public int TotalLines => ChangeMap.Sum(x => x.Value.Lines);
 }
